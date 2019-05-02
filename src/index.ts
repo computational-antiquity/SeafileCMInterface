7308bd599f1a9d23949a7c42663f6e0518e3657b
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2019-05-02 16:04:27 +0200

    wip working example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,98 @@
 import {
-  JupyterLab, JupyterLabPlugin
+  JupyterLab, JupyterLabPlugin, ILayoutRestorer
 } from '@jupyterlab/application';
 import {
-  ICommandPalette
+  ICommandPalette, InstanceTracker
 } from '@jupyterlab/apputils';
+import {
+  JSONExt
+} from '@phosphor/coreutils';
 import {
   Widget
 } from '@phosphor/widgets';
+import {
+  Message
+} from '@phosphor/messaging';
 
 import '../style/index.css';
+
+class SeafileWidget extends Widget {
+ /**
+ Build Seafile Interface Widget
+ **/
+ constructor() {
+   super();
+
+   this.id = 'SeafileInterface';
+   this.title.label = 'Seafile CM Interface';
+   this.title.closable = true;
+   this.addClass('jp-seafileInterfaceWidget');
+
+   this.img = document.createElement('img');
+   this.img.className = 'jp-seafileInterfaceInput';
+   this.node.appendChild(this.img);
+
+   this.img.insertAdjacentHTML('afterend',
+   `
+   <div class="jp-seafileAttribution">
+     <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-seafileAttribution" target="_blank">
+       <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+     </a>
+   </div>
+   `
+   );
+ }
+
+ readonly img: HTMLImageElement;
+
+ onUpdateRequest(msg: Message): void {
+   fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+     return response.json();
+   }).then(data => {
+     this.img.src = data.img;
+     this.img.alt = data.title;
+     this.img.title = data.alt;
+   });
+ }
+};
+
+function activate(app: JupyterLab, palette: ICommandPalette, restorer: ILayoutRestorer) {
+  console.log('JupyterLab extension SeafileContentManagerInterface is activated!');
+
+  // create a widget
+  let widget:  SeafileWidget = new SeafileWidget();
+
+
+  // Add application command
+  const command: string = 'xkcd:open';
+  app.commands.addCommand(command, {
+    label: 'Seafile CM Interface',
+    execute: () => {
+      if (!widget) {
+        widget = new SeafileWidget();
+        widget.update();
+      }
+      if (!tracker.has(widget)) {
+        tracker.add(widget);
+      }
+      if (!widget.isAttached) {
+        // Attach to main work area
+        app.shell.addToMainArea(widget);
+      } else {
+        widget.update();
+      }
+      app.shell.activateById(widget.id);
+    }
+  });
+  palette.addItem({command, category: 'Tutorial'});
+
+  let tracker = new InstanceTracker<Widget>({ namespace: 'seafile'});
+  restorer.restore(tracker, {
+    command,
+    args: () => JSONExt.emptyObject,
+    name: () => 'seafile'
+  });
+};
 
 
 /**
@@ -17,56 +101,8 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'SeafileContentManagerInterface',
   autoStart: true,
-  requires: [ICommandPalette],
-  activate: (app: JupyterLab, palette: ICommandPalette) => {
-    console.log('JupyterLab extension SeafileContentManagerInterface is activated!');
-
-    // create a widget
-    let widget:  Widget = new Widget();
-    widget.id = 'SeafileInterface';
-    widget.title.label = 'Seafile CM Interface';
-    widget.title.closable = true;
-    widget.addClass('jp-seafileInterfaceWidget');
-
-    // add image
-    let img = document.createElement('img');
-    img.className = 'jp-seafileInterfaceInput';
-    widget.node.appendChild(img);
-
-    // attribution
-    img.insertAdjacentHTML('afterend',
-    `
-    <div class="jp-seafileAttribution">
-      <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-seafileAttribution" target="_blank">
-        <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
-      </a>
-    </div>
-    `
-    );
-
-    // fetch info
-    fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
-      return response.json();
-    }).then(data => {
-      img.src = data.img;
-      img.alt = data.title;
-      img.title = data.alt;
-    });
-
-    // Add application command
-    const command: string = 'xkcd:open';
-    app.commands.addCommand(command, {
-      label: 'Seafile CM Interface',
-      execute: () => {
-        if (!widget.isAttached) {
-          // Attach to main work area
-          app.shell.addToMainArea(widget);
-        }
-        app.shell.activateById(widget.id);
-      }
-    });
-    palette.addItem({command, category: 'Tutorial'});
-  }
+  requires: [ICommandPalette, ILayoutRestorer],
+  activate: activate
 };
 
 export default extension;
