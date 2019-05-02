79cb73b20b1531348cf031dbb241178f87613e9c
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2019-05-02 14:47:23 +0200

    wip cm interface panel working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+import {
+  Widget
+} from '@phosphor/widgets';
 
 import '../style/index.css';
 
@@ -11,8 +17,29 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'SeafileContentManagerInterface',
   autoStart: true,
-  activate: (app: JupyterLab) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('JupyterLab extension SeafileContentManagerInterface is activated!');
+
+    // create a widget
+    let widget:  Widget = new Widget();
+    widget.id = 'SeafileInterface';
+    widget.title.label = 'Seafile CM Interface';
+    widget.title.closable = true;
+
+    // Add application command
+    const command: string = 'xkcd:open';
+    app.commands.addCommand(command, {
+      label: 'Seafile CM Interface',
+      execute: () => {
+        if (!widget.isAttached) {
+          // Attach to main work area
+          app.shell.addToMainArea(widget);
+        }
+        app.shell.activateById(widget.id);
+      }
+    });
+    palette.addItem({command, category: 'Tutorial'});
   }
 };
 
